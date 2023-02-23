6af21bff359eef7980d57fe22a6953c7673ee9bc
Alex Zernov <93455802+Omletos@users.noreply.github.com>
2023-02-23 21:23:07 +0000

    Gave the user limited amount of attempts

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#get user input
-guess = input("Guess a word: ").upper()
-print(guess)
+#loop to loop 6 times for 6 guesses
+for guess_num in range(1, 7):
+  guess = input(f"\nGuess {guess_num}: ").upper()#get user input
 
 #test if the user's guess is the same (==) as 'SNAKE'
-if guess == "SNAKE":
-  print("Correct")
-else:
-  print("Wrong")
+  if guess == "SNAKE":
+    print("Correct")
+    break
+  else:
+    print("Wrong")
