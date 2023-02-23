b0e3af16eea49240ae793bb0dcba1e47d6e6dab6
Alex Zernov <93455802+Omletos@users.noreply.github.com>
2023-02-23 21:18:32 +0000

    Comparing input to a word to see if it is correct. Making all chars on input uppercse with .upper().

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-guess = input("Guess a word: ")
+#get user input
+guess = input("Guess a word: ").upper()
 print(guess)
+
+#test if the user's guess is the same (==) as 'SNAKE'
+if guess == "SNAKE":
+  print("Correct")
+else:
+  print("Wrong")
